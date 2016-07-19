9c849d5506c40c2f6dd24b313471638663e6c3d4
Matthias Eichstedt <matthias.eichstedt@mesosphere.io>
2016-07-19 12:38:37 +0200

    Fixed docs

--- src/main/scala/mesosphere/marathon/core/task/termination/TaskKillService.scala
+++ src/main/scala/mesosphere/marathon/core/task/termination/TaskKillService.scala
@@ -13,9 +13,7 @@ trait TaskKillService {
 
   /**
     * Kill the given tasks and return a future that is completed when all of the tasks
-    * have been reported killed. If the list contains tasks that are lost, the implementation
-    * should issue a driver.kill in case the tasks are reachable in the meantime, but should
-    * not wait for them to be reported as being killed.
+    * have been reported as terminal.
     *
     * @param tasks the tasks that shall be killed.
     * @return a future that is completed when all tasks are killed.
@@ -31,7 +29,7 @@ trait TaskKillService {
   def kill(taskId: Task.Id): Unit
 
   /**
-    * Kill the given task. See [[kill(task: Task.Id)]] The implementation should add the task onto
+    * Kill the given task. The implementation should add the task onto
     * a queue that is processed short term and will eventually kill the task.
     *
     * @param task the task that shall be killed.
